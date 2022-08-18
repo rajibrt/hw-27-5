d519258516e07dd4b1de5fa59de678780525defe
rajibrt <rajibrt@gmail.com>
2022-08-18 18:01:04 +0600

    first commit

---
+++ js/3.js
@@ -0,0 +1,20 @@
+const discountButton = document.getElementById('btn-discount').addEventListener('click', function () {
+    const price = document.getElementById('price');
+    const priceString = price.innerText;
+    const mainPrice = parseFloat(priceString);
+    // discount
+    const discountCode = document.getElementById('discount-code');
+    const validCode = discountCode.value;
+    console.log(validCode);
+
+    if (validCode !== 'DOM') {
+        alert('Code not valid')
+        discountCode.value = '';
+        return;
+    }
+    else {
+        const discount = (mainPrice / 100) * 30;
+        const afterDiscount = mainPrice - discount;
+        price.innerText = afterDiscount;
+    }
+})
